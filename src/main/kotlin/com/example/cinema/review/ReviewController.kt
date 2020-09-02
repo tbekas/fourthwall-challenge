04774fc7d8f1be237043a13bf4faff96823cb5ac
Tomasz Bekas <tomasz.bekas@gmail.com>
2020-09-02 16:19:03 +0200

    Adding ReviewController

---
+++ src/main/kotlin/com/example/cinema/review/ReviewController.kt
@@ -0,0 +1,33 @@
+package com.example.cinema.review
+
+import arrow.syntax.collections.prependTo
+import com.example.cinema.movie.Movie
+import com.example.cinema.movie.MovieRepository
+import com.example.cinema.movie.Review
+import com.example.cinema.notFoundIfEmpty
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+
+@RestController
+class ReviewController(val movieRepository: MovieRepository) {
+
+    // TODO once having user context verify if the user hasn't already posted a review
+
+    @PostMapping("/movies/{id}/reviews")
+    fun postReview(@PathVariable("id") movieId: Long, @RequestBody review: Review): Mono<Movie> {
+        return movieRepository.findById(movieId)
+                .notFoundIfEmpty()
+                .map { it.copy(
+                        reviews = review.prependTo(it.reviews),
+                        rating = calcRating(it.rating, it.reviews.size, review.rating)
+                ) }
+                .flatMap { movieRepository.save(it) }
+    }
+
+    fun calcRating(movieRating: Double, reviewCount: Int, reviewRating: Double) =
+            (movieRating * reviewCount + reviewRating) / (reviewCount + 1)
+
+}
